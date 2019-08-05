3c88c2669372a24da69f7960487230f694bf19e8
Stiven75 <01idanut1@mail.ru>
2019-08-05 17:26:29 +0400

    Content

--- Fur/Scripts/script.js
+++ Fur/Scripts/script.js
@@ -1,1 +1,26 @@
-﻿
+﻿function press() {
+    var g;
+    if ($('#max').html() != 0) {
+        g = $('#max').html();
+    }
+    else {
+        g = 1000000;
+    }
+    for (i = 0; i <= g; i++) {
+        if ($("#" + i).prop("checked") == true) {
+            var t = i;
+        }
+
+      
+    }
+
+    $('#content').load('/Basket/Del?Id=' + t);
+    $('#content').load('/Basket/Basket', showNewContent)//Запрос к странице
+    function showNewContent() {
+        $('#content').show('normal');
+    }
+    $('#Kol').load('/Basket/Kol', showNewContent)//Запрос к странице
+    function showNewContent() {
+        $('#Kol').show('normal');
+    }
+}
