022c273c4821a0c2a8c405f116b97f9fb94c83de
Stiven75 <47862792+Stiven75@users.noreply.github.com>
2020-10-25 19:28:18 +0400

    Тест

--- Fur/Models/Category.cs
+++ Fur/Models/Category.cs
@@ -9,6 +9,7 @@ namespace Fur.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+		
         public ICollection<Product> Products { get; set; }
         public Category()
         {
